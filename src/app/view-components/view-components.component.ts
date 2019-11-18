08d6ed0ffee8d7f8dead04074b45752507e1808d
Joao Paulo Peixoto Avila <joao.paulo@zup.com.br>
2019-11-18 16:03:23 -0300

    page to view components

--- src/app/view-components/view-components.component.ts
+++ src/app/view-components/view-components.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-view-components',
   templateUrl: './view-components.component.html',
   styleUrls: ['./view-components.component.scss']
 })
-export class ViewComponentsComponent implements OnInit {
+export class ViewComponentsComponent {
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
